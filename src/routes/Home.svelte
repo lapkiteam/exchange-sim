0ef6b546604fbeed0bff1e292fdbf9fe40752c0c
Agent Lapki <treesortbubble@gmail.com>
2026-06-17 20:21:50 +0300

    feat: make start stage (#7)

---
+++ src/routes/Home.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { push } from "svelte-spa-router"
+
+  import { concat } from "../lib/utils"
+</script>
+
+<div class={concat([
+  "size-full",
+  "flex",
+  "justify-center",
+  "items-center",
+])}>
+  <button
+    class={concat([
+      "bg-blue-500",
+      "p-2",
+    ])}
+    on:click={() => {
+      push("/exchange")
+    }}
+  >
+    Начать обмен
+  </button>
+</div>
