8a0b988e496aed586a9a20c49636c3d3f6301f16
Agent Lapki <treesortbubble@gmail.com>
2026-06-17 19:06:43 +0300

    refactor: extract `Exchange` view (#5)

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,7 @@
   import { concat } from "./lib/utils"
   import type { Inventory } from "./lib/inventory"
   import type { Item } from "./lib/item"
-  import PlayerWindow from "./components/PlayerWindow.svelte"
+  import Exchange from "./components/Exchange.svelte"
 
   const items: Item[] = [
     {
@@ -36,50 +36,21 @@
   <div class={concat([
     "flex",
     "justify-center",
+    "bg-gray-200",
+    "dark:bg-gray-800",
+    "dark:text-white",
   ])}>
     <div class={concat([
       "relative", // for children with absolute
       "w-full",
       "h-dvh",
       "h-[calc(var(--vh,_1vh)_*_100)]",
-      "bg-gray-200",
-      "dark:bg-gray-800",
-      "dark:text-white",
-      "flex",
-      "flex-col",
       "p-2",
-      "gap-3",
     ])}>
-      <div class={concat([
-        "grow",
-        "overflow-y-auto",
-        "size-full",
-      ])}>
-        <PlayerWindow
-          inventory={firstPlayer}
-          agreed={() => {}}
-          disagreed={() => {}}
-        />
-      </div>
-      <div class={concat([
-        "flex",
-        "justify-center",
-      ])}>
-        <hr class={concat([
-          "w-[80%]",
-        ])} />
-      </div>
-      <div class={concat([
-        "grow",
-        "overflow-y-auto",
-        "size-full",
-      ])}>
-        <PlayerWindow
-          inventory={secondPlayer}
-          agreed={() => {}}
-          disagreed={() => {}}
-        />
-      </div>
+      <Exchange
+        firstPlayer={firstPlayer}
+        secondPlayer={secondPlayer}
+      />
     </div>
   </div>
 </main>
